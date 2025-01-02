b1d0145ccf0272efea9e4a284fe37180f3d7428c
TnKatt <berkatsiallagan201105@gmail.com>
2025-01-03 00:21:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
-<?php
-// Redirect user to the login page
-header("Location: ../Server APPRIM/auth/login.php");
-exit;
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
