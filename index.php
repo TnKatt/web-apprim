77aa7b243b7b7dbcec504b8f64b87a62adbd5032
TnKatt <berkatsiallagan201105@gmail.com>
2025-01-03 00:24:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Halo
-</body>
-</html>
+<?php
+// Redirect user to the login page
+header("Location: ../auth/login.php");
+exit;
+?>
