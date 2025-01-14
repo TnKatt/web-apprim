c5953bc2627a417f536e6c4336a7f9925dd197d5
Adhyca <154980407+FeatingRates@users.noreply.github.com>
2025-01-14 21:53:17 +0700

    Delete back-end directory

--- back-end/admin/tambah-ruang.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".submit-btn").addEventListener("click", function(event) {
-    document.getElementById("loading-popup").style.display = "flex";
-    setTimeout(function() {
-        window.location.href = "../admin/daftar-ruang.php?id_gedung=<?= $id_gedung ?>";
-    }, 3000);
-});
